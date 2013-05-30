6741fcb7bc67b4c4d38d6b262d0b29b2299b32e0
p000ison <maximilian.ammann@googlemail.com>
2013-05-30 13:10:53 +0200

    Fixed NPE in test

--- src/test/java/CommandTest.java
+++ src/test/java/CommandTest.java
@@ -68,7 +68,7 @@ public class CommandTest {
         Command subsubCommand = executor.build(this, "SubSubTest");
         executor.register(this, "Test").addSubCommand(subCommand);
         subCommand.addSubCommand(subsubCommand);
-        executor.register(new HelpCommand(executor, "Help", "helpme", "help",  "help", null));
+        executor.register(new HelpCommand(executor, "Help", "helpme", "help",  "help", "ids-args-usage"));
     }
 
     public static void main(String[] args) {
